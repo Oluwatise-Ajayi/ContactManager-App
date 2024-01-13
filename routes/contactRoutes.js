fd6741948911a12397e08bb9cad4dee4edc186bc
Ajayi Oluwatise <dr393462@gmail.com>
2024-01-13 21:02:33 +0100

    added controllers

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,20 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const {getContact,createContact,getOneContact,updateContact,deleteContact} = require("../controllers/contactControllers")
 
-router.route("/").get((req, res) => {
-  res.status(200).json({ message: "Get all contacts" });
-});
-router.route("/").post((req, res) => {
-  res.status(200).json({ message: "Create new contacts" });
-});
-router.route("/:id").get((req, res) => {
-  res.status(200).json({ message: `Get contact for ${req.params.id}` });
-});
-router.route("/:id").put((req, res) => {
-  res.status(200).json({ message: `Update contact for ${req.params.id}` });
-});
-router.route("/:id").delete((req, res) => {
-  res.status(200).json({ message: `delete contact for ${req.params.id}` });
-});
+router.route("/").get(getContact).post(createContact);
+router.route("/:id").get(getOneContact).put(updateContact).delete(deleteContact);
 
 module.exports = router;
